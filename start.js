0cc04e0017ec64523dcbe6e3be97edf7fcba9d76
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-09-08 17:33:03 +0800

    feat: 初始化一些目录并集成ejs模板

---
+++ start.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/api', require('./apis/index.js'))
+app.use('/', require('./router/index.js'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
