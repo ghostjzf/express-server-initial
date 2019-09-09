2665837b4a0f426182c6dc63f8238050a877de1f
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-09-09 14:44:45 +0800

    feat(mysql): 抽象mysql调用函数

--- apis/index.js
+++ apis/index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../utils/mysql");
 
-router.get('/admin', (req, res) => {
-	res.json({code: 200, data: 'admin', msg: 'ok'})
-})
+router.get("/admin", (req, res) => {
+    db.query("SELECT * FROM user", [], function(result, fields) {
+        console.log("查询结果：");
+        console.log(result);
+    });
+
+    res.json({ code: 200, data: "admin", msg: "ok" });
+});
 
 module.exports = router;
