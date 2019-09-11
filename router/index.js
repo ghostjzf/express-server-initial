3670d2eb7051ff056c409d7163cf989b0b5d9ce3
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-09-11 13:27:36 +0800

    引入css文件

--- router/index.js
+++ router/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.render('index', {title: 'express & ejs'})
-})
+router.get("/", (req, res) => {
+    res.render("index", { title: "Hello Express & Ejs" });
+});
 
 module.exports = router;
